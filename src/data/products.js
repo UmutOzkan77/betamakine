13cf783a8563eb9b4037834e6db859365d8781c1
Umut Özkan <86379071+UmutOzkan77@users.noreply.github.com>
2026-01-20 01:17:06 +0300

    Delete src directory

--- src/data/products.js
+++
@@ -1,5 +0,0 @@
-const productModules = import.meta.glob('./product-items/*.js', { eager: true });
-
-export const products = Object.values(productModules)
-  .map(module => module.default)
-  .sort((a, b) => a.id - b.id);
